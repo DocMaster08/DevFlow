5ea9d6b6b8b35e2c5b39d50bfd728e674192b996
doclag <ademelhamadi505@gmail.com>
2026-07-31 18:12:36 +0100

    extract task logic into custom hook

--- client/src/pages/TasksPage.tsx
+++ client/src/pages/TasksPage.tsx
@@ -1,40 +1,16 @@
 import TaskForm from "@/features/tasks/components/TaskForm"
 import TaskList from "@/features/tasks/components/TaskList"
-import { mockTasks } from "@/features/tasks/utils/mockTasks"
-import type { Task } from "@/types/task"
-import { useState } from "react"
+import { useTasks } from "@/features/tasks/hooks/useTasks"
 
 function TasksPage() {
-    const [tasks, setTasks] = useState(mockTasks)
 
-    function handleDeleteTask(id: string) {
-        setTasks(tasks.filter(task => task.id !== id))
-    }
-
-    function handleToggleTask(id: string) {
-        setTasks(tasks.map(task => {
-            return task.id === id ? { ...task, status: task.status === "done" ? "todo" : "done" } : task
-        }))
-    }
-
-    function handleCreateTask(title: string) {
-        const newTask: Task = {
-            id: crypto.randomUUID(),
-            title,
-            status: "todo",
-            priority: "low",
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
-
-        setTasks([...tasks, newTask]);
-    }
+    const { tasks, createTask, toggleTask, deleteTask } = useTasks();
 
     return (
         <div className="space-y-6">
             <h1>Tasks</h1>
-            <TaskForm onCreateTask={handleCreateTask} />
-            <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} onToggleTask={handleToggleTask} />
+            <TaskForm onCreateTask={createTask} />
+            <TaskList tasks={tasks} onDeleteTask={deleteTask} onToggleTask={toggleTask} />
         </div>
     )
 }
